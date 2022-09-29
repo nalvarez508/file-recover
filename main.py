221feb474b456f721942532beb5c2891b2cce0dc
nalvarez508 <nickalvarez189@gmail.com>
2022-09-29 14:31:23 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from recovery.listwindow import ListWindow
+from wizard.window import Wizard
+
+if __name__ == "__main__":
+  # data = SetupWizard()
+  # if data != None:
+  #   ListWindow(data)
+  Wizard()
