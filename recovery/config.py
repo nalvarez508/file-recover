c569eab4e3a86da6c4bf981e4400898002953c2e
nalvarez508 <nickalvarez189@gmail.com>
2022-09-30 09:43:23 -0700

    Add messages

--- recovery/config.py
+++ recovery/config.py
@@ -1,6 +1,12 @@
 import os
 
-WELCOME_MSG = "Welcome to the unofficial DiskDigger recovery wizard. You'll be guided through the setup process and will be able to select which files to save at the end. Please ensure you've completed a DiskDigger scan and saved the report (via the Advanced tab) to somewhere accessible.\n\nClick Next to begin."
+WELCOME_MSG = "Welcome to the unofficial DiskDigger recovery wizard. You'll be guided through the setup process and will be able to select which files to save at the end. Please ensure you've completed a DiskDigger scan and saved the report (via the Advanced tab) to somewhere accessible."
+FILE_SELECT_MSG = "Select the DiskDigger report file."
+DRIVE_SELECT_MSG = "Select the drive where the files will be recovered from."
+WARNING_MSG = "WARNING: Using the affected drive, such as writing a file, may render your data unrecoverable."
+
+PADX = 16
+LABEL_SIZE = 16
 
 if os.name == 'nt':
   BACKGROUND = 'SystemButtonFace'
