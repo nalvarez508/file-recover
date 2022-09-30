415375ceb63d753f885943dc58bc7ff7e0b99139
nalvarez508 <nickalvarez189@gmail.com>
2022-09-30 09:44:15 -0700

    driver

--- main.py
+++ main.py
@@ -2,7 +2,4 @@ from recovery.listwindow import ListWindow
 from wizard.window import Wizard
 
 if __name__ == "__main__":
-  # data = SetupWizard()
-  # if data != None:
-  #   ListWindow(data)
-  Wizard()
+  ListWindow()
